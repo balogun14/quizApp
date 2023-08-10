4af896599641d57e207a7a48d73aa71ecf75945f
balogun14 <awwalbalogun06@gmail.com>
2023-08-10 08:02:31 +0100

    splitted the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: avoid_print,use_const, prefer_const_constructors
 
+import 'package:calculator/result.dart';
 import 'package:flutter/material.dart';
-import 'package:calculator/question.dart';
-import 'answer.dart';
+import 'quiz.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,40 +15,47 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-
-  void answerQuestion() {
+  final _questions = const [
+    {
+      'questionText': 'What\'s Your favorite colour?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Rabbit', 'Goat']
+    },
+    {
+      'questionText': 'What\'s your favorite Movie?',
+      'answers': ['Barbie', 'Oppenheimer', 'The Originals', 'Mario']
+    },
+  ];
+  void _answerQuestion() {
     setState(() {
       _questionIndex += 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have More Questions');
+    } else {
+      print('No more questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s Your favorite colour?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Rabbit', 'Goat']
-      },
-    ];
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('My First App'),
-            ),
-            body: Column(
-              children: <Widget>[
-                Question(
-                  questions[_questionIndex]['questionText'] as String,
-                ),
-                ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-                  return Answer(name: answer, selectHandler: answerQuestion,);
-                }).toList(),
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionindex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
+    );
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:calculator/question.dart';
+import 'package:flutter/material.dart';
+
+import 'answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final Function answerQuestion;
+  final int questionindex;
+  const Quiz(
+      {super.key,
+      required this.questions,
+      required this.answerQuestion,
+      required this.questionindex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(
+          questions[questionindex]['questionText'] as String,
+        ),
+        ...(questions[questionindex]['answers'] as List<String>).map((answer) {
+          return Answer(
+            name: answer,
+            selectHandler: answerQuestion,
+          );
+        }).toList(),
+      ],
+    );
+  }
+}
